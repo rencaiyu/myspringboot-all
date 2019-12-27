dff6f01a443f3dd5aef1bb09f0db1dab744460e5
rencaiyu <1223356413@qq.com>
2019-12-27 14:32:22 +0800

    easyPoi

---
+++ springboot-easypoi/src/main/java/com/rcy/springbooteasypoi/SpringbootEasypoiApplication.java
@@ -0,0 +1,13 @@
+package com.rcy.springbooteasypoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEasypoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEasypoiApplication.class, args);
+	}
+
+}
