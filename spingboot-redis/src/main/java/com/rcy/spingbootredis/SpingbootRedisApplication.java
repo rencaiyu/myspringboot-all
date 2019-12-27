d32fa5d77b1187920762269dd1513bea7315fe92
rencaiyu <1223356413@qq.com>
2019-12-27 14:24:18 +0800

    Initial commit

---
+++ spingboot-redis/src/main/java/com/rcy/spingbootredis/SpingbootRedisApplication.java
@@ -0,0 +1,13 @@
+package com.rcy.spingbootredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootRedisApplication.class, args);
+	}
+
+}
---
+++ spingboot-redis/src/test/java/com/rcy/spingbootredis/SpingbootRedisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rcy.spingbootredis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpingbootRedisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
