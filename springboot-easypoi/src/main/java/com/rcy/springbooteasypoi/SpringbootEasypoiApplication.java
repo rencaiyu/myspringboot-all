d038086abf3600db8da8b748eb83856d4e9d39d5
rencaiyu <1223356413@qq.com>
2019-12-30 15:06:49 +0800

    easy 基于大数据的导出

--- springboot-easypoi/src/main/java/com/rcy/springbooteasypoi/SpringbootEasypoiApplication.java
+++ springboot-easypoi/src/main/java/com/rcy/springbooteasypoi/SpringbootEasypoiApplication.java
@@ -1,9 +1,11 @@
 package com.rcy.springbooteasypoi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.rcy.springbooteasypoi.mapper")
 public class SpringbootEasypoiApplication {
 
 	public static void main(String[] args) {
--- springboot-easypoi/src/main/java/com/rcy/springbooteasypoi/controller/EasyPoiExportController.java
+++ springboot-easypoi/src/main/java/com/rcy/springbooteasypoi/controller/EasyPoiExportController.java
@@ -4,7 +4,10 @@ import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
 import cn.afterturn.easypoi.excel.entity.enmus.ExcelType;
 import com.rcy.springbooteasypoi.bean.StudentEntity;
+import com.rcy.springbooteasypoi.bean.TreePO;
+import com.rcy.springbooteasypoi.mapper.TreePOMapper;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,7 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @description: 基于Easy POI的 web 导出
@@ -22,6 +27,9 @@ import java.util.Date;
 @Controller
 @RequestMapping("/easyPOI")
 public class EasyPoiExportController {
+
+    @Autowired
+    TreePOMapper treePOMapper;
 
     /**
      * @param request
@@ -45,8 +53,6 @@ public class EasyPoiExportController {
         studentEntityList.add(studentEntity);
         studentEntityList.add(studentEntityOne);
 
-
-
         // 告诉浏览器用什么软件可以打开此文件
         response.setHeader("content-Type", "application/vnd.ms-excel");
         // 下载文件的默认名称
@@ -58,4 +64,29 @@ public class EasyPoiExportController {
         workbook.write(response.getOutputStream());
 
     }
+
+    /**
+     * @param request
+     * @param response
+     * @throws Exception
+     * @description: 基于Easy POI的webd导出   大数据量
+     */
+    @RequestMapping("/exportExcelBigData")
+    public void exportExcelBigData(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        List<TreePO> treePoList = treePOMapper.selectAll();
+        ExportParams params = new ExportParams("大数据测试", "测试");
+        Workbook workbook = ExcelExportUtil.exportBigExcel(params, TreePO.class, treePoList);
+        ExcelExportUtil.closeExportBigExcel();
+
+        // 告诉浏览器用什么软件可以打开此文件
+        response.setHeader("content-Type", "application/vnd.ms-excel");
+        // 下载文件的默认名称
+        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode("此Excel", "UTF-8") + ".xls");
+        //编码
+        response.setCharacterEncoding("UTF-8");
+        workbook.write(response.getOutputStream());
+    }
+
+
 }
